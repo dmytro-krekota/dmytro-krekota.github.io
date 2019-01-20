4a72d9e6d4f0c0593d644a06861702a8d32eda2d
Dmytro Krekota <dmytro.krekota@gmail.com>
2019-01-21 01:27:49 +0200

    Add angular app

---
+++ src/app/dashboard/dashboard.module.ts
@@ -0,0 +1,11 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { DashboardComponent } from './dashboard.component';
+
+@NgModule({
+  declarations: [DashboardComponent],
+  imports: [BrowserModule],
+  providers: [],
+  bootstrap: [],
+})
+export class DashboardModule { }
